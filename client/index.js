7ebd52e31123a4544a9dae79801000f10838e23e
Shohboz <vip.shohboz@gmail.com>
2017-11-06 20:12:37 +0300

    i18n

--- client/index.js
+++ client/index.js
@@ -3,8 +3,9 @@ import { render } from "react-dom";
 import BrowserRouter from "react-router-dom/BrowserRouter";
 import { Provider } from "react-redux";
 import { renderRoutes } from "react-router-config";
-import { IntlProvider } from "react-intl";
+import { addLocaleData, IntlProvider } from "react-intl";
 import Cookie from "js-cookie";
+import fetch from "isomorphic-fetch";
 import { configureStore } from "shared/redux/configureStore";
 import routes from "shared/routes";
 import "shared/initializers";
@@ -12,20 +13,39 @@ import "shared/initializers";
 const preloadedState = window.__PRELOADED_STATE__;
 delete window.__PRELOADED_STATE__;
 
-const locale = Cookie.get("locale") || "en";
-
 const store = configureStore(preloadedState);
 
-const AppRouter = () => {
-  return (
-    <IntlProvider locale={locale}>
-      <Provider store={store}>
-        <BrowserRouter>
-          {renderRoutes(routes)}
-        </BrowserRouter>
-      </Provider>
-    </IntlProvider>
-  );
-};
+const locale = Cookie.get("locale") || "en";
 
-render(<AppRouter />, document.querySelector("#root"));
+const AppRouter = ({ messages, locale }) => (
+  <IntlProvider
+    locale={locale}
+    initialNow={parseInt(window.INITIAL_NOW, 10)}
+    messages={messages}
+  >
+    <Provider store={store}>
+      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    </Provider>
+  </IntlProvider>
+);
+
+fetch(`/public/assets/${locale}.json`)
+  .then(res => {
+    if (res.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+
+    return res.json();
+  })
+  .then(localeData => {
+    addLocaleData(window.ReactIntlLocaleData[locale]);
+
+    return localeData;
+  })
+  .catch(e => console.error(e))
+  .then(messages => {
+    render(
+      <AppRouter messages={messages} locale={locale} />,
+      document.querySelector("#root")
+    );
+  });
